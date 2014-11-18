cb8ae49e5ad0bf4db9cf6bb93d2037333318c852
naoya-ogawa <example@co.jp>
2014-11-18 22:51:54 +0900

    add column attribute

---
+++ NpgLinq/MyAttribute/SqlColumnAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NpgLinq
+{
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = false)]
+    public class SqlColumnAttribute : System.Attribute
+    {
+        public string Name { get; set; }
+        public bool IsPrimaryKey { get; set; }
+    }
+}
