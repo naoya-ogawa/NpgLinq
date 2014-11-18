789dd1a8f72481011a376a9f5281f645b34a5082
naoya-ogawa <example@co.jp>
2014-11-18 22:57:46 +0900

    add table attribute

---
+++ NpgLinq/MyAttribute/SqlTableAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NpgLinq
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, AllowMultiple = false)]
+    public class SqlTableAttribute : System.Attribute
+    {
+        public string Name { get; set; }
+    }
+}
--- NpgLinq/NpgExtension.cs
+++ NpgLinq/NpgExtension.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace NpgLinq
 {
-    public class Class1
+    public static class NpgExtension
     {
+
     }
 }
