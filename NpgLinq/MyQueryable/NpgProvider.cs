32d8b6873dc00926c0952abbe5a161e95b8d48fd
naoya-ogawa <example@co.jp>
2014-11-19 00:20:47 +0900

    add query provider

---
+++ NpgLinq/MyQueryable/NpgProvider.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NpgLinq
+{
+    public class NpgQueryProvider : IQueryProvider
+    {
+        protected NpgQueryProvider()
+        {
+        }
+
+        IQueryable<S> IQueryProvider.CreateQuery<S>(Expression expression)
+        {
+            //return null;
+            return new NpgQuery<S>(this, expression);
+        }
+
+        IQueryable IQueryProvider.CreateQuery(Expression expression)
+        {
+            Type elementType = TypeSystem.GetElementType(expression.Type);
+
+            try
+            {
+                return (IQueryable)Activator.CreateInstance(typeof(NpgQuery<>)
+                    .MakeGenericType(elementType), new object[] { this, expression });
+            }
+            catch (TargetInvocationException tie)
+            {
+                throw tie.InnerException;
+            }
+        }
+
+        S IQueryProvider.Execute<S>(Expression expression)
+        {
+            return (S)this.Execute(expression);
+        }
+
+        object IQueryProvider.Execute(Expression expression)
+        {
+            return this.Execute(expression);
+        }
+
+        public string GetQueryText(Expression expression)
+        {
+            return string.Empty;
+        }
+        public object Execute(Expression expression)
+        {
+            return null;
+        }
+    }
+
+
+}
